06e86fe2f84fccd2c19c43d1d1516f43b0245f6f
Bruno <brunoantunes94@hotmail.com>
2021-05-19 02:43:36 -0300

    Criação do formulário
    Nessa etapa foi realizada a primeira fase da criação do formulário.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import React from 'react';
 import Section2 from './Component/section2.jsx';
 import FinalPage from './Component/final.jsx';
+import Inicio from './Component/inicio.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="App-header">
+        <Inicio />
+      </div>
       <div className="mt-5">
         <Section2 />
       </div>
